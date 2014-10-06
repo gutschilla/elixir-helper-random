fb801765651e8e6fa02866803592eade96b6c4d7
Martin Gutsch <gutsch@zwoelf.net>
2014-10-06 15:14:23 +0200

    seed random umber generator before every use

--- lib/random.ex
+++ lib/random.ex
@@ -1,5 +1,21 @@
 defmodule Random do
 
+    use Application
+
+    # See http://elixir-lang.org/docs/stable/elixir/Application.html
+    # for more information on OTP Applications
+    def start(_type, _args) do
+        import Supervisor.Spec, warn: false
+
+        children = [
+          # Define workers and child supervisors to be supervised
+          # worker(TestApp.Worker, [arg1, arg2, arg3])
+        ]
+        opts = [strategy: :one_for_one, name: Random.Supervisor]
+        Supervisor.start_link(children, opts)
+    end
+
+
     def string( length ) do
         get_string( length )
     end
@@ -9,6 +25,7 @@ defmodule Random do
     end
 
     def get_string( length ) do
+        :random.seed(:erlang.now())
         alphabet
             =  "abcdefghijklmnopqrstuvwxyz"
             <> "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -29,10 +46,24 @@ defmodule Random do
     end
 
     def get_number( length ) do
+        :random.seed(:erlang.now())
         { number, "" } = Integer.parse 1..length |> Enum.map_join fn(_) ->
             :random.uniform(10) - 1
         end
         number
     end
 
+end
+
+defmodule Random.Supervisor do
+    use Supervisor
+
+    def start_link do
+        Supervisor.start_link(__MODULE__, :ok)
+    end
+
+    def init(:ok) do
+        children = []
+        supervise(children, strategy: :one_for_one)
+    end
 end
